a46f902ae762a6fc79686ace780b2eaf122983ec
mingi <meengi07@gmail.com>
2021-08-17 09:13:24 +0900

    게시판 , 게시글작성, 게시글수정, 삭제, 게시글조회 구현완료

--- src/com/kh/board/model/dao/BoardDao.java
+++ src/com/kh/board/model/dao/BoardDao.java
@@ -8,12 +8,15 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Properties;
+
+import com.kh.board.model.vo.Board;
 
 public class BoardDao {
 
+	private Board board = new Board();
 	private Connection conn;
-	private PreparedStatement pstmt;
 	private ResultSet rs;
 	private Properties prop = new Properties();
 	
@@ -72,23 +75,123 @@ public class BoardDao {
 		try {
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, getNext());
+			pstmt.setString(2, nickName);
+			pstmt.setString(3, board.getTagName());
+			pstmt.setString(4, boardTitle);
+			pstmt.setDate(5, getDate());
+			pstmt.setString(6, boardContent);
 			
+			return pstmt.executeUpdate();
 			
-			
-			
-			
-			
-			rs = pstmt.executeQuery();
-			if(rs.next()) {
-				return rs.getInt(1) + 1;
-			}
-			return 1; //첫번째 게시물인 경우 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		return 0;
+		return -1;
 	}
-
+	public ArrayList<Board> getList(int pageNumber){
+		String sql = prop.getProperty("getList");
+		ArrayList<Board> list = new ArrayList<Board>();
+		
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, getNext() - (pageNumber -1) * 10); 
+			
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				Board board = new Board();
+				board.setBoardNo(rs.getInt(1));
+				board.setNickName(rs.getString(2));
+				board.setTagName(rs.getString(3));
+				board.setBoardTitle(rs.getString(4));
+				board.setBoardDate(rs.getDate(5));
+				board.setBoardContent(rs.getString(6));
+				list.add(board);
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return list; //데이터베이스 오류 
+		
+	}
+	public boolean nextPage(int pageNumber) {
+		String sql = prop.getProperty("nextPage");
+		
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, getNext() - (pageNumber -1) * 10); 
+			
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false; 
+	}
+	public Board getBoard(int boardNo) {
+		String sql = prop.getProperty("getBoard");
+		
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, boardNo); 
+			
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				Board board = new Board();
+				board.setBoardNo(rs.getInt(1));
+				board.setNickName(rs.getString(2));
+				board.setTagName(rs.getString(3));
+				board.setBoardTitle(rs.getString(4));
+				board.setBoardDate(rs.getDate(5));
+				board.setBoardContent(rs.getString(6));
+				return board;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null; 
+		
+	}
+	public int boardUpdate(int boardNo, String boardTitle, String boardContent) {
+		String sql = prop.getProperty("boardUpdate");
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, boardTitle);
+			pstmt.setString(2, boardContent);
+			pstmt.setInt(3, boardNo);
+			
+			return pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return -1;
+	}
+	public int boardDelete(int boardNo) {
+		String sql = prop.getProperty("boardDelete");
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, boardNo);
+			
+			return pstmt.executeUpdate();
+			 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return -1;
+	}
 }
