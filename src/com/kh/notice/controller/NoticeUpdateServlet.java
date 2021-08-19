3ecfc7f09ae6f21764f5c6f59d3d4bdeb4f9d2d5
KHY0705 <khyaa0705@gmail.com>
2021-08-19 21:47:01 +0900

    2021-08-19 Notice 상세보기/추가/삭제/수정 완료(정상동작)

--- src/com/kh/notice/controller/NoticeUpdateServlet.java
+++ src/com/kh/notice/controller/NoticeUpdateServlet.java
@@ -1,6 +1,8 @@
 package com.kh.notice.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,16 +13,16 @@ import com.kh.notice.model.service.NoticeService;
 import com.kh.notice.model.vo.Notice;
 
 /**
- * Servlet implementation class NoticeReadServlet
+ * Servlet implementation class NoticeUpdateServlet
  */
-@WebServlet("/read.no")
-public class NoticeReadServlet extends HttpServlet {
+@WebServlet("/update.no")
+public class NoticeUpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public NoticeReadServlet() {
+    public NoticeUpdateServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -29,18 +31,26 @@ public class NoticeReadServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int nno=Integer.parseInt(request.getParameter("nno"));
-		Notice notice = new NoticeService().selectNotice(nno);
+		int nno = Integer.parseInt(request.getParameter("nno"));
 		
-		String view = "";
-		if(notice != null) {
-			request.setAttribute("notice", notice);
-			view = "views/notice/noticeReadView.jsp";
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		
+		Notice n = new Notice();
+		n.setNoticeNo(nno);
+		n.setNoticeTitle(title);
+		n.setNoticeContent(content.replaceAll("\n", "<br>"));
+		
+		int result = new NoticeService().updateNotice(n);
+		
+		if(result > 0) {
+			request.getSession().setAttribute("msg", "공지사항이 성공적으로 수정되었습니다.");
+			response.sendRedirect("noticeView.no?nno="+nno);
 		}else {
-			request.setAttribute("msg", "공지사항 조회에 실패하였습니다.");
-			view = "views/common/errPage.jsp";
+			request.getSession().setAttribute("msg", "공지사항 수정 실패");
+			RequestDispatcher view = request.getRequestDispatcher("views/common/errorPage.jsp");
+			view.forward(request, response);
 		}
-		request.getRequestDispatcher(view).forward(request, response);
 	}
 
 	/**
