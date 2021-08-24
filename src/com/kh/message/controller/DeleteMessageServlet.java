2716bd9a9254f325bdcb771d2bb07500b979eeed
DH_furaha <80407403+pastelto@users.noreply.github.com>
2021-08-24 11:25:45 +0900

    01.리스트 수정

---
+++ src/com/kh/message/controller/DeleteMessageServlet.java
@@ -0,0 +1,43 @@
+package com.kh.message.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kh.member.model.vo.Member;
+
+/**
+ * Servlet implementation class DeleteMessageServlet
+ */
+@WebServlet("/dmsg.ms")
+public class DeleteMessageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteMessageServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 유저아이디 넘기기 
+				String userId = ((Member)request.getSession().getAttribute("loginUser")).getUserId();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
